72a595feacb51bf17ba7a7220e1e7822960e643c
Mauricio Davila <Mauro@MacBook-Pro-de-Mauricio.local>
2017-03-05 20:23:47 -0300

    Serial Printf Primer commit

--- ARDUINO/SERIAL_PRINTF/SERIAL_PRINTF.ino
+++ ARDUINO/SERIAL_PRINTF/SERIAL_PRINTF.ino
@@ -1,12 +1,52 @@
+/******************************************************************
+* Programa: Ejemplo de uso del Puerto Serie
+*
+* Objetivo:
+*   Mostrar los aspectos básicos del funcionamiento del Puerto Serie
+*
+* Aspectos a destacar:
+*   -Escritura por el puerto serie
+*   -Funcion que recibe argumentos variables
+*   -Utilidad del puerto como log
+*
+* Versión: 0.1 del 5 de marzo de 2017
+* Autor: Mauricio Dávila
+* Revisión: -
+*
+* *******************************************************************/
 
 #include "stdarg.h";
-#include <stdio.h>;
+
+void serialPrintf(char *text, ...);
 
 
+/** \brief  Se invoca una sola vez cuando el programa empieza. 
+ *          Se utiliza para inicializar los modos de trabajo 
+ *          de los pines, el puerto serie, etc... 
+ * \param void
+ * \return void
+ */
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); // Setea la velocidad del puerto Serie
 }
 
+
+/** \brief  Contiene el programa que se ejecutará cíclicamente
+ *          
+ * \param void
+ * \return void
+ */
+void loop() 
+{
+  char auxiliar[]= "JUAN";
+  serialPrintf("HOLA %s\n",auxiliar);
+  delay(1000);
+}
+
+/** \brief  Emula el funcionamiento del printf.
+ * \param Recibe un texto seguido por argumentos variables
+ * \return void
+ */
 void serialPrintf(char *text, ...)
 {
   char aux[100];
@@ -15,12 +55,4 @@ void serialPrintf(char *text, ...)
   vsprintf(aux,text,ap);
   va_end(ap);
   Serial.print(aux);
-}
-
-
-void loop() 
-{
-  char auxiliar[]= "JUAN";
-  serialPrintf("HOLA %s\n",auxiliar);
-  delay(1000);
 }
