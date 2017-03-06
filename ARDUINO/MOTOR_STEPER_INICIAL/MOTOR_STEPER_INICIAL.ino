9b84dae7656f04aefbcdb56a68b60e3cc369c7ee
Mauricio Davila <Mauro@MacBook-Pro-de-Mauricio.local>
2017-03-06 20:24:30 -0300

    MOTOR_STEP primer commit

---
+++ ARDUINO/MOTOR_STEPER_INICIAL/MOTOR_STEPER_INICIAL.ino
@@ -0,0 +1,116 @@
+
+/******************************************************************
+* Programa: Ejemplo de uso del motor paso a paso
+*
+* Objetivo:
+*   Mostrar los aspectos básicos de funcionamiento de un paso a paso
+*
+* Aspectos a destacar:
+*   -Escritura de salidas digitales
+
+*
+* Versión: 0.1 del 6 de marzo de 2017
+* Autor: 
+* http://www.geeetech.com/wiki/index.php/Stepper_Motor_5V_4-Phase_5-Wire_%26_ULN2003_Driver_Board_for_Arduino
+* Revisión: -
+*
+* *******************************************************************/
+
+int Pin0 = 10; 
+int Pin1 = 11; 
+int Pin2 = 12; 
+int Pin3 = 13; 
+int _step = 0; 
+boolean dir = true;
+
+/** \brief  Se invoca una sola vez cuando el programa empieza. 
+ *          Se utiliza para inicializar los modos de trabajo 
+ *          de los pines, el puerto serie, etc... 
+ * \param void
+ * \return void
+ */
+void setup() 
+{ 
+ pinMode(Pin0, OUTPUT);  
+ pinMode(Pin1, OUTPUT);  
+ pinMode(Pin2, OUTPUT);  
+ pinMode(Pin3, OUTPUT);  
+} 
+
+
+/** \brief  Contiene el programa que se ejecutará cíclicamente
+ *          
+ * \param void
+ * \return void
+ */
+ void loop() 
+{ 
+ switch(_step){ 
+   case 0: 
+     digitalWrite(Pin0, LOW);  
+     digitalWrite(Pin1, LOW); 
+     digitalWrite(Pin2, LOW); 
+     digitalWrite(Pin3, HIGH); 
+   break;  
+   case 1: 
+     digitalWrite(Pin0, LOW);  
+     digitalWrite(Pin1, LOW); 
+     digitalWrite(Pin2, HIGH); 
+     digitalWrite(Pin3, HIGH); 
+   break;  
+   case 2: 
+     digitalWrite(Pin0, LOW);  
+     digitalWrite(Pin1, LOW); 
+     digitalWrite(Pin2, HIGH); 
+     digitalWrite(Pin3, LOW); 
+   break;  
+   case 3: 
+     digitalWrite(Pin0, LOW);  
+     digitalWrite(Pin1, HIGH); 
+     digitalWrite(Pin2, HIGH); 
+     digitalWrite(Pin3, LOW); 
+   break;  
+   case 4: 
+     digitalWrite(Pin0, LOW);  
+     digitalWrite(Pin1, HIGH); 
+     digitalWrite(Pin2, LOW); 
+     digitalWrite(Pin3, LOW); 
+   break;  
+   case 5: 
+     digitalWrite(Pin0, HIGH);  
+     digitalWrite(Pin1, HIGH); 
+     digitalWrite(Pin2, LOW); 
+     digitalWrite(Pin3, LOW); 
+   break;  
+     case 6: 
+     digitalWrite(Pin0, HIGH);  
+     digitalWrite(Pin1, LOW); 
+     digitalWrite(Pin2, LOW); 
+     digitalWrite(Pin3, LOW); 
+   break;  
+   case 7: 
+     digitalWrite(Pin0, HIGH);  
+     digitalWrite(Pin1, LOW); 
+     digitalWrite(Pin2, LOW); 
+     digitalWrite(Pin3, HIGH); 
+   break;  
+   default: 
+     digitalWrite(Pin0, LOW);  
+     digitalWrite(Pin1, LOW); 
+     digitalWrite(Pin2, LOW); 
+     digitalWrite(Pin3, LOW); 
+   break;  
+ } 
+ if(dir){ 
+   _step++; 
+ }else{ 
+   _step--; 
+ } 
+ if(_step>7){ 
+   _step=0; 
+ } 
+ if(_step<0){ 
+   _step=7; 
+ } 
+ delay(30); 
+}
